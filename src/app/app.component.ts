5032de667c9645d7cdea797a5d919e1d03ff1359
V-link <v-link@LTA1L5IWY8-MAC.local>
2018-03-05 17:10:06 +0530

    adding pattern in pie chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   trendsData:any = { 
+    total: 100,
     withIndueData : 81,
     overDue : 19,
     unit: 'pecentage'
--- src/app/ngChart/components/pie-chart/pie-chart.component.ts
+++ src/app/ngChart/components/pie-chart/pie-chart.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, Renderer2, ViewChild, AfterViewInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
   styleUrls: ['./pie-chart.component.scss']
 })
-export class PieChartComponent implements OnInit {
+export class PieChartComponent implements OnInit, AfterViewInit {
   @Input() data: any;
-  constructor() { }
+  @ViewChild('pieCircle') circle: ElementRef;
+  constructor(private _eleRef: ElementRef, private _render: Renderer2) {
+   }
+  setPieChart():void {
+    let circumfrence = 2*(22/7)*this.circle.nativeElement.getAttribute('r');
+    let result = (this.data.overDue*circumfrence/100) + ' ' + circumfrence;
+    this._render.setStyle(this.circle.nativeElement, 'strokeDasharray', result);
+    this._render.setStyle(this.circle.nativeElement, 'stroke', 'url(#pattern-line)');
+  }
+  displayTooltip():void {
+    /**bind mouseover event to show tooltio */
+    this._render.listen(this.circle.nativeElement, 'mouseover', function($event){
+      console.log('>>>'+$event.pageX+'px');
+      console.log('>>>'+$event.pageY+'px');
+    })
+    /**bind mouseover event to show tooltio */
+    this._render.listen(this.circle.nativeElement, 'mouseout', function($event){
+      
+    })
+  }
   ngOnInit() {
     console.log(this.data)
+  }
+  ngAfterViewInit() {
+    console.log(this.circle)
+    this.setPieChart()
+    this.displayTooltip();
   }
 
 }
